d9b75d681496780d31cb4145d0619d327b5d1334
Evgeniy Schemelinin <33124785+Kovarniy@users.noreply.github.com>
2019-05-20 18:00:12 +0800

    final version

---
+++ res.php
@@ -0,0 +1,33 @@
+<?php
+        // Получаем содержимое файла config.json
+        // Допустим, там закодированный массив вида
+        // array('apple', 'orange')
+        $json = file_get_contents('order.json');
+        // Декодируем
+        $json = json_decode($json, true);
+        // Добавляем элемент
+        $json[] = (object) array('name' => $_POST["name"],
+                                 'phone' => $_POST["myPhone"]
+         );
+
+        // Превращаем опять в JSON
+        $json = json_encode($json);
+
+        //включаем блокировку
+        if (flock($handler, LOCK_EX)) {
+            fwrite($handler, $text);
+            //сняли блокировку
+            flock($handler, LOCK_UN);
+        } else {
+            //блокировку выполнить не удалось
+            echo "Не могу выполнить блокировку!";
+        }
+
+?>
+
+<?php
+           // Перезаписываем файл
+           $handler = file_put_contents('order.json', $json);
+           if ($handler  === false)
+                  echo "При сохранении данных произошла ошибка!";
+?>
--- scripts/landing.js
+++ scripts/landing.js
@@ -13,29 +13,21 @@ $(function() {
 });
 
 function sendmessage(id) {
-    alert(id);
 
-    if (id = 'form1') {
-        msg = $('#form1').serialize();
-    } else if (id = 'form2') {
-        msg = $('#form2').serialize();
+    if (id == 'desktop1') {
+        msg = $('#desktop1').serialize();
+    } else if (id == 'mobile1') {
+        msg = $('#mobile1').serialize();
     }
     $.ajax({
-        url: 'https://Kovarniy.github.io/scripts/res.php', // путь к php-обработчику
+        url: 'https://kovarniy.github.io/res.php', // путь к php-обработчику
         type: 'POST', // метод передачи данных
-        // type: 'POST',
-        //url: 'res.php',
         data: msg,
         success: function (data) {
-            alert(msg);
-        },
-        beforeSend: function () { // Функция вызывается перед отправкой запроса
-            alert('Спасибо, мы обязательно свяжемся с вами!');
+            alert('Спасибо мы обязательно свяжемся с вами');
         }
-
     })
 }
-
 
 function myPrice(jsonObj) {
     $(jsonObj.post).each(function (index) {
@@ -47,9 +39,6 @@ function myPrice(jsonObj) {
         $('.price-' + i + '-i').attr('src', images[i]);
         $('.price-' + i + '-p').text(text[i]);
     }
-
-    //$('.price-1-p').attr('src', 'breeds/' + animDogsLink[n] +  '.jpg');
-    //$("#show").load('breeds/' + animDogsLink[n] + '.html');
 }
 
 $(window).scroll(function() {
